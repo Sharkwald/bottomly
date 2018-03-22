c0383ac3296b4dbdb1676a95f76a8590fde5ca92
Owen Morgan-Jones <owen.morganjones@gmail.com>
2018-03-22 08:27:47 +0000

    Fixes equality assertion issue

--- tests/model_tests/test_karma.py
+++ tests/model_tests/test_karma.py
@@ -7,13 +7,18 @@ from model.karma import Karma, KarmaType
 
 class TestKarma(unittest.TestCase):
     def test_persistence(self):
-        # Arrange
+        # Set up
         Config().connect_to_db()
+        old_karma = Karma.objects.all()
+        for ok in old_karma:
+            ok.delete()
+
+        # Arrange
         awarded_to = "testUser1"
         awarded_by = "testUser2"
         test_reason = "testReason"
         test_awarded = datetime.today()
-        test_type = KarmaType.POZZYPOZ
+        test_type = str(KarmaType.POZZYPOZ)
         k = Karma(awarded_to_username=awarded_to,
                   reason=test_reason,
                   awarded_by_username=awarded_by,
@@ -25,7 +30,11 @@ class TestKarma(unittest.TestCase):
 
         # Assert
         loaded_karma = Karma.objects.all()[0]
-        self.assertEqual(k, loaded_karma)
+        self.assertEqual(k.awarded_to_username, loaded_karma.awarded_to_username)
+        self.assertEqual(k.reason, loaded_karma.reason)
+        self.assertEqual(k.awarded_by_username, loaded_karma.awarded_by_username)
+        self.assertAlmostEqual(k.awarded.date(), loaded_karma.awarded.date()) # yukcy but...
+        self.assertEqual(k.karma_type, loaded_karma.karma_type)
 
         # Tear down
         k.delete()
