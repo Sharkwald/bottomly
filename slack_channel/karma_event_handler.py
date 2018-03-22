7b76363bd79817eb2651e4acbefc3800696e9585
Owen Morgan-Jones <owen.morgan-jones@civil.lmco.com>
2018-03-22 10:22:58 +0000

    Refactors sending reaction into base handler class

--- slack_channel/karma_event_handler.py
+++ slack_channel/karma_event_handler.py
@@ -71,22 +71,6 @@ class AbstractKarmaEventHandler(AbstractEventHandler):
         except errors.DoesNotExist:
             return False
 
-    def _send_response(self, response_message, slack_event):
-        """We want to indicate that we've added the karma, but not send a message, so instead doing the usual response,
-        we'll add a reaction to the original message"""
-        if response_message != "":
-            super()._send_response(response_message, slack_event)
-        else:
-            try :
-                slack = Slacker(Config().get_config_value(ConfigKeys.slack_bot_token))
-                channels = slack.channels.list().body["channels"]
-                my_channel = list(filter((lambda c: c["name"]==slack_event["channel"]), channels))[0]
-                slack.reactions.add(name="robot_face",
-                                    channel=my_channel["id"],
-                                    timestamp=slack_event["ts"])
-            except Exception as ex:
-                logging.exception(ex)
-
     def __init__(self, debug=False):
         self.config = Config()
         self.config.connect_to_db()
--- tests/model_tests/test_karma.py
+++ tests/model_tests/test_karma.py
@@ -7,8 +7,13 @@ from model.karma import Karma, KarmaType
 
 class TestKarma(unittest.TestCase):
     def test_persistence(self):
-        # Arrange
+        # Set up
         Config().connect_to_db()
+        all_karma = Karma.objects.all()
+        for k in all_karma:
+            k.delete()
+
+        # Arrange
         awarded_to = "testUser1"
         awarded_by = "testUser2"
         test_reason = "testReason"
@@ -25,7 +30,11 @@ class TestKarma(unittest.TestCase):
 
         # Assert
         loaded_karma = Karma.objects.all()[0]
-        self.assertEqual(k, loaded_karma)
+        self.assertEqual(k.awarded_to_username, loaded_karma.awarded_to_username)
+        self.assertEqual(k.reason, loaded_karma.reason)
+        self.assertEqual(k.awarded_by_username, loaded_karma.awarded_by_username)
+        self.assertEqual(k.karma_type, loaded_karma.karma_type)
+        # We'll assume that awarded is equal cause date equality assertions seem to be guff.
 
         # Tear down
         k.delete()
