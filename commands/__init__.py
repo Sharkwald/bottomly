04a42ef60fba0c5e85c2b26254fb5d95371708b8
Owen Morgan-Jones <owen.morgan-jones@civil.lmco.com>
2018-03-30 10:20:35 +0100

    Adds new command for retriving karma reasons

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,3 +4,4 @@ from .urban_search import UrbanSearchCommand
 from .wikipedia_search import WikipediaSearchCommand
 from .add_karma import AddKarmaCommand
 from .get_current_net_karma import GetCurrentNetKarmaCommand
+from .get_current_karma_reasons import GetCurrentKarmaReasonsCommand
