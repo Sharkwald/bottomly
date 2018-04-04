60e47560f678f71446c8c8838c3fdbb7398d74ec
Owen Morgan-Jones <owen.morgan-jones@civil.lmco.com>
2018-04-04 15:08:25 +0100

    Adds giphy command w/ tests and API key dependency

--- commands/__init__.py
+++ commands/__init__.py
@@ -5,3 +5,4 @@ from .wikipedia_search import WikipediaSearchCommand
 from .add_karma import AddKarmaCommand
 from .get_current_net_karma import GetCurrentNetKarmaCommand
 from .get_current_karma_reasons import GetCurrentKarmaReasonsCommand
+from .giphy_translate import GiphyTranslateCommand
