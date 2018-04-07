ea3b176e13cfee43ddc3a708d086f6e57299278c
Owen Morgan-Jones <owen.morganjones@gmail.com>
2018-04-07 08:09:09 +0100

    Adds some error handling around initial memberlist pop

--- slack_channel/initial_memberlist_populator.py
+++ slack_channel/initial_memberlist_populator.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+import logging
 import os
 from slacker import Slacker
 
@@ -11,19 +13,22 @@ token = Config().get_config_value(ConfigKeys.slack_bot_token)
 class InitialMemberlistPopulator(object):
 
     def populate(self):
-        Config().connect_to_db()
-        slack = Slacker(token)
-        response = slack.users.list()
-        users = response.body['members']
-        data = ""
-        for user in users:
-            if not user['deleted']:
-                username = user['name']
-                slack_id = user['id']
-                c = AddMemberCommand(username, slack_id)
-                c.execute()
-                data += "Added " + username + os.linesep
-        return data
+        try:
+            Config().connect_to_db()
+            slack = Slacker(token)
+            response = slack.users.list()
+            users = response.body['members']
+            data = ""
+            for user in users:
+                if not user['deleted']:
+                    username = user['name']
+                    slack_id = user['id']
+                    c = AddMemberCommand(username, slack_id)
+                    c.execute()
+                    data += "Added " + username + os.linesep
+            return data
+        except Exception:
+            logging.exception("Error running initial populate")
 
     def __init__(self):
         super(InitialMemberlistPopulator, self)
