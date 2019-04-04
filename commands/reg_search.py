e8fcd9bd25a5c60a4966e51bd5418d63f54f46eb
Jamie <jamieprentice@Jamies-MacBook-Air.local>
2019-04-04 23:44:05 +0100

    More, better tests for reg search

--- commands/reg_search.py
+++ commands/reg_search.py
@@ -1,16 +1,28 @@
 from commands.abstract_command import AbstractCommand
 
+
 class RegSearchCommand(AbstractCommand):
 
-
     def get_purpose(self):
-        return "AutoTrader reg lookup, because Jamie is lazy."
+        return 'AutoTrader reg lookup, because Jamie is lazy.'
 
     def execute(self, search_term):
         if search_term is None or search_term == '':
-            return None
+            return 'Registration missing'
 
-        base_url = "https://www.vehiclecheck.co.uk/?vrm="
+        search_term = search_term.replace(' ', '')
+        search_term = search_term.lower()
+
+        if len(search_term) > 7:
+            return 'Registration too long.'
+
+        if not search_term.isalnum():
+            return 'Registration should not contain special characters'
+
+        # Sanity checks done, now replacing common mistakes
+        search_term = search_term.replace('i', '1')
+
+        base_url = 'https://www.vehiclecheck.co.uk/?vrm='
 
         return base_url + search_term
 
--- tests/command_tests/test_reg_search.py
+++ tests/command_tests/test_reg_search.py
@@ -1,25 +1,45 @@
 import unittest
+
 from commands.reg_search import RegSearchCommand
 
 
 class TestUrbanSearchCommand(unittest.TestCase):
+
     def test_empty_input(self):
         command = RegSearchCommand()
         result = command.execute('')
 
-        self.assertIsNone(result)
+        self.assertEqual(result, 'Registration missing')
 
-    def test_garbage_input(self):
+    def test_input_too_long(self):
         command = RegSearchCommand()
-        result = command.execute('asdhbhklasdvfbuioasvfbhuilasdvfbhjkl')
+        result = command.execute('a random string long string')
 
-        self.assertEqual(result, "https://www.vehiclecheck.co.uk/?vrm=asdhbhklasdvfbuioasvfbhuilasdvfbhjkl")
+        self.assertEqual(result, 'Registration too long.')
+
+    def test_input_has_Special_Characters(self):
+        command = RegSearchCommand()
+        result = command.execute('Registration should not contain special characters')
+
+        self.assertEqual(result, 'Registration too long.')
 
     def test_valid_input(self):
         command = RegSearchCommand()
-        result = command.execute("ACarReg")
+        result = command.execute('a car reg')
 
-        self.assertEqual(result, "https://www.vehiclecheck.co.uk/?vrm=ACarReg")
+        self.assertEqual(result, 'https://www.vehiclecheck.co.uk/?vrm=acarreg')
+
+    def test_valid_input_upper_case(self):
+        command = RegSearchCommand()
+        result = command.execute('VR 00M')
+
+        self.assertEqual(result, 'https://www.vehiclecheck.co.uk/?vrm=vr00m')
+
+    def test_valid_input_i_to_1(self):
+        command = RegSearchCommand()
+        result = command.execute('ABC iIi')
+
+        self.assertEqual(result, 'https://www.vehiclecheck.co.uk/?vrm=abc111')
 
 
 if __name__ == '__main__':
