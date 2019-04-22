81ea4ccdfc4d73d9203920c04d921717a7edb267
Owen Morgan-Jones <owen.1morgan-jones@leidos.com>
2019-04-22 13:18:24 +0100

    Tidies a few things

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 from config import Config, ConfigKeys
 from slack_channel.slack_event_handler import SlackEventHandler
 
+live_mode = "live"
+
 if __name__ == '__main__':
-    config = Config()
-    env_mode = config.get_config_value(ConfigKeys.env_key)
-    is_debug = env_mode != "live"
+    is_debug = Config().get_config_value(ConfigKeys.env_key) != live_mode
     handler = SlackEventHandler(is_debug)
     handler.handle_slack_context()
