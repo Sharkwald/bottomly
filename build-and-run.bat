48afd32438c067a741673c63fb84f8a1bbc9bf18
Owen Morgan-Jones <owen.1morgan-jones@leidos.com>
2019-04-22 14:40:42 +0100

    improves container flow!

--- build-and-run.bat
+++
@@ -1,3 +0,0 @@
-docker build --tag docker-bottomly .
-docker system prune -f
-docker run --rm --name bottomly --env-file .env docker-bottomly
--- prep_container_image.ps1
+++ prep_container_image.ps1
@@ -1,3 +1,5 @@
+docker build --tag docker-bottomly .
+docker system prune -f
 az acr login --name BottomlyContainerReg
 $tag = git log --pretty=format:%h -n 1
 docker tag docker-bottomly bottomlycontainerreg.azurecr.io/docker-bottomly:$tag
