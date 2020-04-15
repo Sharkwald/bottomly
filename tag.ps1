8b93b774bd0868a096ea17fa56f79eef47ef687f
Owen Morgan-Jones <owen.1.morgan-jones@leidos.com>
2020-04-15 10:44:08 +0100

    update to tagging script

--- tag.ps1
+++ tag.ps1
@@ -1,5 +1,5 @@
 az acr login --name BottomlyContainerReg
 $tag = git log --pretty=format:%h -n 1
-docker tag docker-bottomly bottomlycontainerreg.azurecr.io/docker-bottomly:$tag
+docker tag bottomly bottomlycontainerreg.azurecr.io/docker-bottomly:$tag
 docker push bottomlycontainerreg.azurecr.io/docker-bottomly:$tag
 docker images
