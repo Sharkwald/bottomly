da252b2ff09d992fe804c46a7a89ab5286bd902c
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2022-05-27 17:13:49 +0100

    Cocktail of the week lookup

---
+++ commands/coctail_of_the_week_saerch.py
@@ -0,0 +1,49 @@
+from commands.abstract_command import AbstractCommand
+
+import os
+import requests
+from bs4 import BeautifulSoup
+
+
+class CocktailOfTheWeekSearchCommand(AbstractCommand):
+
+    def get_purpose(self):
+        return 'Cocktail of the week, a reminder we were once fun.'
+
+    def execute(self):
+        base_url = 'https://thebrassmonkeygla.co.uk/'
+
+        response = requests.get(base_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        try:
+            # Header Row - Used to identify
+            header_row = soup.find(text='Cocktail of the Week').parent.parent.parent
+
+            # Blank Row - Because fuck restaurant websites
+            blank_row = header_row.find_next('tr')
+
+            # Name & Price Row
+            name_row = blank_row.find_next('tr')
+            name_row_data_cells = name_row.findAll('td')
+
+            # Name
+            name = name_row_data_cells[0].text
+            # Price
+            price = name_row_data_cells[1].text
+
+            # Description Row
+            ingredients_row = name_row.find_next('tr')
+            ingredients = ingredients_row.find('td').text
+
+            return 'Cocktail of the Week' + os.linesep + name + ' - (' + price + ')' + os.linesep + ingredients
+
+        except:
+            error_element = soup.find('div', {'class': 'ErrorMessage'})
+            error_message = error_element.findChild('h3').text
+
+            return error_message
+
+
+def __init__(self):
+    super(CocktailOfTheWeekSearchCommand, self)
---
+++ tests/command_tests/test_cocktail_of_the_week_search.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from commands.coctail_of_the_week_saerch import CocktailOfTheWeekSearchCommand
+
+
+class TestCocktailOfTheWeekSearchCommand(unittest.TestCase):
+
+    def test_empty_input(self):
+        command = CocktailOfTheWeekSearchCommand()
+        result = command.execute()
+
+        expected = 'Cocktail of the Week' + os.linesep + 'Raspberry Gin Cosmo - (£7.00)' + os.linesep + 'Whitley Neil Raspberry Gin, Triple Sec, Lime Juice, Cranberry'
+        self.assertEqual(expected, result)
+
+if __name__ == '__main__':
+    unittest.main()
