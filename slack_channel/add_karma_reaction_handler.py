43cf23ba7474359e26537607d988db1b0457f115
Owen.Morgan-Jones <owen.morganjones@hymans.co.uk>
2022-05-30 13:34:24 +0000

    Adds handling for reaction emoji skintones

--- slack_channel/add_karma_reaction_handler.py
+++ slack_channel/add_karma_reaction_handler.py
@@ -26,14 +26,19 @@ class AddKarmaReactionHandler(AbstractReactionHandler):
         return AddKarmaCommand()
     
     def can_handle(self, reaction_add_event) -> bool:
-        return reaction_add_event["reaction"] in _karma_reactions
+        reaction = self.parse_reaction(reaction_add_event["reaction"])
+        return reaction in _karma_reactions
     
     def _invoke_handler_logic(self, reaction_add_event):
         try:
+            reaction = self.parse_reaction(reaction_add_event["reaction"])
             self.command.execute(awarded_to=reaction_add_event["reactee"],
                                  awarded_by=reaction_add_event["reactor"],
-                                 reason="Reacted with " + reaction_add_event["reaction"],
-                                 karma_type=_karma_reactions[reaction_add_event["reaction"]])
+                                 reason="Reacted with " + reaction,
+                                 karma_type=_karma_reactions[reaction])
             self._send_reaction_response(reaction_add_event)
         except Exception as ex:
             logging.exception(ex)
+
+    def parse_reaction(self, raw_reaction):
+        return raw_reaction.split('::')[0]
