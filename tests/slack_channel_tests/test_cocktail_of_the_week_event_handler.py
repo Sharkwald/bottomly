5f1a5482f3677a53d1472bdd81bba895668a3246
Jamie Prentice <2858046+JamiePrentice@users.noreply.github.com>
2022-05-30 12:47:19 +0100

    More tests

--- tests/slack_channel_tests/test_cocktail_of_the_week_event_handler.py
+++ tests/slack_channel_tests/test_cocktail_of_the_week_event_handler.py
@@ -12,7 +12,7 @@ test_prefix = "_"
 valid_event = {"text": test_prefix + "cotw"}
 invalid_event = {"text": "this is missing a valid command prefix"}
 help_event = {"text": test_prefix + "cotw -?"}
-urban_response = "Doubtless some pile of filth"
+cotw_response = "Water. (£0.00). Water"
 
 
 @patch.object(Config, "get_config_value")
@@ -24,43 +24,43 @@ class TestCocktailOfTheWeekEventHandler(unittest.TestCase):
         can_handle = handler.can_handle(valid_event)
         self.assertTrue(can_handle)
 
-    # def test_does_not_handle_different_event(self, prefix_method, config_method):
-    #     handler = UrbanEventHandler()
-    #     can_handle = handler.can_handle(invalid_event)
-    #     self.assertFalse(can_handle)
+    def test_does_not_handle_different_event(self, prefix_method, config_method):
+        handler = CocktailOfTheWeekEventHandler()
+        can_handle = handler.can_handle(invalid_event)
+        self.assertFalse(can_handle)
 
-    # @patch.object(UrbanEventHandler, "_send_message_response")
-    # @patch.object(UrbanSearchCommand, "execute", return_value = urban_response)
-    # def test_command_execute_is_called(self, execute_method, response_method, config_method, prefix_method):
-    #     handler = UrbanEventHandler()
-    #     handler.handle(valid_event)
-    #     execute_method.assert_called_once_with(valid_event["text"][4:])
+    @patch.object(CocktailOfTheWeekEventHandler, "_send_message_response")
+    @patch.object(CocktailOfTheWeekSearchCommand, "execute", return_value=cotw_response)
+    def test_command_execute_is_called(self, execute_method, response_method, config_method, prefix_method):
+        handler = CocktailOfTheWeekEventHandler()
+        handler.handle(valid_event)
+        execute_method.assert_called_once_with()
 
-    # @patch.object(UrbanSearchCommand, "execute", return_value=urban_response)
-    # @patch.object(UrbanEventHandler, "_send_message_response")
-    # def test_command_result_is_correctly_built(self, response_method, execute_method, config_method, prefix_method):
-    #     handler = UrbanEventHandler()
-    #     handler.handle(valid_event)
-    #     response_method.assert_called_once_with(urban_response, valid_event)
+    @patch.object(CocktailOfTheWeekSearchCommand, "execute", return_value=cotw_response)
+    @patch.object(CocktailOfTheWeekEventHandler, "_send_message_response")
+    def test_command_result_is_correctly_built(self, response_method, execute_method, config_method, prefix_method):
+        handler = CocktailOfTheWeekEventHandler()
+        handler.handle(valid_event)
+        response_method.assert_called_once_with(cotw_response, valid_event)
 
-    # @patch.object(UrbanSearchCommand, "execute", return_value=None)
-    # @patch.object(UrbanEventHandler, "_send_message_response")
-    # def test_no_result_message_correctly_sent(self, response_method, execute_method, config_method, prefix_method):
-    #     handler = UrbanEventHandler()
-    #     handler.handle(valid_event)
-    #     empty_results_message = "Left as an exercise for the reader."
-    #     response_method.assert_called_once_with(empty_results_message, valid_event)
+    @patch.object(CocktailOfTheWeekSearchCommand, "execute", return_value=None)
+    @patch.object(CocktailOfTheWeekEventHandler, "_send_message_response")
+    def test_no_result_message_correctly_sent(self, response_method, execute_method, config_method, prefix_method):
+        handler = CocktailOfTheWeekEventHandler()
+        handler.handle(valid_event)
+        empty_results_message = "You're on your own I'm afraid - https://thebrassmonkeygla.co.uk/."
+        response_method.assert_called_once_with(empty_results_message, valid_event)
 
-    # @patch.object(UrbanSearchCommand, "get_purpose", return_value="Urbans")
-    # @patch.object(UrbanEventHandler, "_send_message_response")
-    # def test_get_usage(self, response_method, purpose_method, config_method, prefix_method):
-    #     handler = UrbanEventHandler()
-    #     handler.handle(help_event)
-    #     expected_help = "Urban Dictionary" + os.linesep + \
-    #                     "Urbans"+ os.linesep +\
-    #                     "Usage: `" + test_prefix + "ud <query>" + "`"
-    #     purpose_method.assert_called_once_with()
-    #     response_method.assert_called_once_with(expected_help, help_event)
+    @patch.object(CocktailOfTheWeekSearchCommand, "get_purpose", return_value="Cocktail of the Week")
+    @patch.object(CocktailOfTheWeekEventHandler, "_send_message_response")
+    def test_get_usage(self, response_method, purpose_method, config_method, prefix_method):
+        handler = CocktailOfTheWeekEventHandler()
+        handler.handle(help_event)
+        expected_help = "Cocktail of the week lookup from the Brass Monkey" + os.linesep + \
+                        "Cocktail of the Week" + os.linesep + \
+                        "Usage: `" + test_prefix + "cotw " + "`"
+        purpose_method.assert_called_once_with()
+        response_method.assert_called_once_with(expected_help, help_event)
 
     if __name__ == '__main__':
         unittest.main()
