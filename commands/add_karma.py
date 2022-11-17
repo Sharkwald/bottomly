d85e76bb3c2a66ef4567f131a3800280c6f88f04
Owen.Morgan-Jones <owen.morganjones@hymans.co.uk>
2022-11-17 16:17:59 +0000

    Moves karma reaction dictionary to command layer

--- commands/add_karma.py
+++ commands/add_karma.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from commands.abstract_command import AbstractCommand
 from config import Config
-from model.karma import Karma
+from model.karma import Karma, KarmaType
 
 
 class AddKarmaCommand(AbstractCommand):
@@ -21,6 +21,31 @@ class AddKarmaCommand(AbstractCommand):
 
         k.save()
         return k
+
+    @staticmethod
+    def get_karma_reactions() -> dict:
+        return {
+            "+1": KarmaType.POZZYPOZ,
+            "arrow_up": KarmaType.POZZYPOZ,
+            "clap": KarmaType.POZZYPOZ,
+            "heart": KarmaType.POZZYPOZ,
+            "heart_eyes": KarmaType.POZZYPOZ,
+            "heavy_plus_sign": KarmaType.POZZYPOZ,
+            "heavy_tick": KarmaType.POZZYPOZ,
+            "joy": KarmaType.POZZYPOZ,
+            "party_parrot": KarmaType.POZZYPOZ,
+            "raised_hands": KarmaType.POZZYPOZ,
+            "smile": KarmaType.POZZYPOZ,
+            "thumbsup": KarmaType.POZZYPOZ,
+            "-1": KarmaType.NEGGYNEG,
+            "arrow_down": KarmaType.NEGGYNEG,
+            "hankey": KarmaType.NEGGYNEG,
+            "heavy_minus_sign": KarmaType.NEGGYNEG,
+            "poo": KarmaType.NEGGYNEG,
+            "poop": KarmaType.NEGGYNEG,
+            "shit": KarmaType.NEGGYNEG,
+            "thumbsdown": KarmaType.NEGGYNEG
+        }
 
     def __init__(self):
         super(AddKarmaCommand, self)
